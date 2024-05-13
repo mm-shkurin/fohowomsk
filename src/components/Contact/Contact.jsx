747276f66e3625a2d7055728ecd221cc082546e9
mm-shkurin <trape3977@gmail.com>
2024-05-13 16:03:16 +0600

    .

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,24 +1,26 @@
-import './Contact.css'
+import React, { useEffect, useRef } from 'react';
+import './Contact.css';
+
 function MapWithMarker() {
-    const mapRef = useRef(null);
+const mapRef = useRef(null);
 
-    useEffect(() => {
-        if (!mapRef.current && window.DG && window.DG.Map && window.DG.marker) {
-            // После загрузки скрипта 2ГИС API инициализируем карту
-            const map = new window.DG.Map('map', {
-                center: [54.98659 , 73.3788], // координаты центра карты 54°59'11.9
-                zoom: 16 // начальное масштабирование карты
-            });
-            mapRef.current = map;
+useEffect(() => {
+if (!mapRef.current && window.DG && window.DG.Map && window.DG.marker) {
+// После загрузки скрипта 2ГИС API инициализируем карту
+const map = new window.DG.Map('map', {
+center: [54.98659, 73.3788], // координаты центра карты 54°59'11.9
+zoom: 16 // начальное масштабирование карты
+});
+mapRef.current = map;
 
-            // Добавляем метку на карту
-            const marker = new window.DG.marker([54.98659 , 73.3788]).addTo(map);
-        } else {
-            console.error('2GIS API is not loaded or marker object is not defined.');
-        }
-    }, []);
+// Добавляем маркер на карту
+const marker = new window.DG.marker([54.98659, 73.3788]).addTo(map);
+} else {
+console.error('2GIS API не загружен или объект маркера не определен.');
+}
+}, []);
 
-    return <div className='map' id="map" style={{ width: '500px', height: '400px' }}></div>;
+return <div className='map' id="map" style={{ width: '500px', height: '400px' }}></div>;
 }
 
 export default MapWithMarker;
